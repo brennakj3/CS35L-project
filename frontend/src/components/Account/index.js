58cc644b8f96d0cdb4a81532605172b09517a42a
Maximus1729 <maxwelllipshutz@gmail.com>
2022-11-28 09:13:26 -0800

    The account page now shows reviews given by the user.

--- frontend/src/components/Account/index.js
+++ frontend/src/components/Account/index.js
@@ -1,12 +1,58 @@
-
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom';
+import Review from '../Review'
+ 
+ 
 function Account(){
-    //So far just has a button to let the user log out
-
+    const [reviews, setReviews] = useState([]);
+    var userReviews = [];
+    
+    // This useEffect hook gets all reviews made by the given user
+    useEffect(() => {
+        async function getReviews() {
+            const response = await fetch('http://localhost:5000/getReviews');
+            
+            // in case of fetching error
+            if (!response.ok) {
+                const message = `An error occurred: ${response.statusText}`;
+                console.log(message);
+                return;
+            }
+            
+            const reviews = await response.json();
+            console.log(reviews);
+            setReviews(reviews);
+        }
+        getReviews();
+    }, []);
+    
+    function showReviews() {
+        reviews.map((review) => {
+            var reviewUser = review.user;
+            
+            if (reviewUser === sessionStorage.getItem('user')) {
+                userReviews.push(review);
+                console.log(userReviews);
+            }
+            
+            return review;
+        });
+        
+        if(userReviews.length !== 0) {
+            return userReviews.map((review) => {
+               return(
+                   <Review review = {review}/>
+               );
+            });
+        }
+    }
     
     return(
-      <div></div>
+        <div className='account'>
+           <p>{showReviews()}</p>
+        </div>
     );
 }
 export default Account;
+
