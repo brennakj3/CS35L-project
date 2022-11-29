4de8e9e9ff73ca15381a0ff91c007b5aa5a86829
Maximus1729 <maxwelllipshutz@gmail.com>
2022-11-29 04:48:15 -0800

    Added sort by restaurant name and rating to the account page

--- frontend/src/components/Account/index.js
+++ frontend/src/components/Account/index.js
@@ -3,10 +3,9 @@ import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom';
 import Review from '../Review'
  
- 
 function Account(){
     const [reviews, setReviews] = useState([]);
-    var userReviews = [];
+    const [userReviews, setUserReviews] = useState([]);
     
     // This useEffect hook gets all reviews made by the given user
     useEffect(() => {
@@ -21,36 +20,107 @@ function Account(){
             }
             
             const reviews = await response.json();
-            console.log(reviews);
             setReviews(reviews);
+            
+            // filters the list of reviews for only those made by the current user
+            var tempReviews = [];
+            reviews.map((review) => {
+                var reviewUser = review.user;
+                
+                if (reviewUser === sessionStorage.getItem('user'))
+                    tempReviews.push(review);
+                
+                return review;
+            });
+            
+            // sort by restaurant (default sorting)
+            let count = 0;
+            do {
+                count = 0;
+                for (let i = 0; i < tempReviews.length - 1; i++) {
+                    if (tempReviews[i].diningHall > tempReviews[i + 1].diningHall) {
+                        let temp = tempReviews[i];
+                        tempReviews[i] = tempReviews[i + 1];
+                        tempReviews[i + 1] = temp;
+                        count++;
+                    }
+                }
+            } while (count > 0);
+            
+            setUserReviews(tempReviews);
         }
         getReviews();
     }, []);
     
-    function showReviews() {
+    const sortReviews = (event) => {
+        // filters the list of reviews for only those made by the current user
+        var tempReviews = [];
         reviews.map((review) => {
             var reviewUser = review.user;
             
-            if (reviewUser === sessionStorage.getItem('user')) {
-                userReviews.push(review);
-                console.log(userReviews);
-            }
+            if (reviewUser === sessionStorage.getItem('user'))
+                tempReviews.push(review);
             
             return review;
         });
         
+        // sort alphabetically by restaurant name
+        var sortType = event.target.value;
+        if (sortType === "restaurant") {
+            let count = 0;
+            do {
+                count = 0;
+                for (let i = 0; i < tempReviews.length - 1; i++) {
+                    if (tempReviews[i].diningHall > tempReviews[i + 1].diningHall) {
+                        let temp = tempReviews[i];
+                        tempReviews[i] = tempReviews[i + 1];
+                        tempReviews[i + 1] = temp;
+                        count++;
+                    }
+                }
+            } while (count > 0);
+        }
+        
+        // sort by rating (highest to lowest)
+        if (sortType === "rating") {
+            let count = 0;
+            do {
+                count = 0;
+                for (let i = 0; i < tempReviews.length - 1; i++) {
+                    if (tempReviews[i].rating < tempReviews[i + 1].rating) {
+                        let temp = tempReviews[i];
+                        tempReviews[i] = tempReviews[i + 1];
+                        tempReviews[i + 1] = temp;
+                        count++;
+                    }
+                }
+            } while (count > 0);
+        }
+        
+        setUserReviews(tempReviews);
+        console.log(userReviews);
+    }
+    
+    // shows the review HTML elements
+    function showReviews() {
         if(userReviews.length !== 0) {
-            return userReviews.map((review) => {
-               return(
-                   <Review review = {review}/>
-               );
+            const test = userReviews.map((review) => {
+                return(
+                       <Review review={review}/>
+                );
             });
+            console.log(test)
+            return test;
         }
     }
     
     return(
-        <div className='account'>
-           <p>{showReviews()}</p>
+        <div className="menu">
+            <select className="menuSelect" name="Sort by" onChange={sortReviews}>
+                <option className="option" value="restaurant">By Restaurant</option>
+                <option className="option" value="rating">Highest Rated</option>
+            </select>
+            <p>{showReviews()}</p>
         </div>
     );
 }
